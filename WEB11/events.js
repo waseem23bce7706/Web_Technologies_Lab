505eb386e54d8825272d93839d532e78533a144a
waseem23bce7706 <waseem.23bce7706@vitapstudent.ac.in>
2026-04-08 23:06:27 +0530

    Delete WEB11 directory

--- WEB11/events.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on('greet', (name) => {
-    console.log(`Hello ${name}`);
-});
-
-eventEmitter.emit('greet', 'Mokshitha');
